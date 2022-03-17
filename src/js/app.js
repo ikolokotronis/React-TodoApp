ac9212077fd0545a62cd29b0e8440d607cf0faa9
ikolokotronis <ioanniskolokotronis1@gmail.com>
2022-03-17 11:57:46 +0100

    Webpack config

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,13 @@
+import React from 'react';
+import ReactDOM from "react-dom";
 
+function App(props) {
+    return (
+        <div>Test</div>
+    );
+}
 
-
+ReactDOM.render(
+    <App/>,
+    document.getElementById('app')
+)
