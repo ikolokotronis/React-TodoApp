c7325712358678279f136c337af628637e35e764
ikolokotronis <ioanniskolokotronis1@gmail.com>
2022-03-21 12:55:44 +0100

    update App component

--- src/js/app.js
+++ src/js/app.js
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import ReactDOM from "react-dom";
+import {getTasks} from "./api/tasks";
+import {getOperations} from "./api/operations";
+import NewTask from "./NewTask";
 
 function App(props) {
+    const [taskData, setTaskData] = useState([])
+    const [updateData, setUpdateData] = useState(0)
+
+    useEffect(()=>{
+        getTasks((data)=>{
+            setTaskData(data)
+        })
+        console.log(taskData)
+    }, [updateData])
+
     return (
-        <div>Test</div>
+        <NewTask onNewTask={()=>setUpdateData(state=>state+1)}/>
     );
 }
 
