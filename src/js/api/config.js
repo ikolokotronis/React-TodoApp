f846d2b57becb427bc79f7d1f38ec7ec2840e058
ikolokotronis <ioanniskolokotronis1@gmail.com>
2022-03-21 11:43:52 +0100

    First fetching methods

--- src/js/api/config.js
+++ src/js/api/config.js
@@ -1,2 +1,2 @@
-const API_KEY = '1a890b17-d9f6-414d-a157-3fbb956ad83c'
-const API_URL = 'https://todo-api.coderslab.pl/api'
+export const API_KEY = '1a890b17-d9f6-414d-a157-3fbb956ad83c'
+export const API_URL = 'https://todo-api.coderslab.pl/api'
--- src/js/app.js
+++ src/js/app.js
@@ -10,4 +10,4 @@ function App(props) {
 ReactDOM.render(
     <App/>,
     document.getElementById('app')
-)
+);
